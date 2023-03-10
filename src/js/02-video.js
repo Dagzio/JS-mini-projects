ff83332c4a2283cc9ff42e1938bd5bfd1d786758
Valentine Pylypchuk <strheadshot1997@gmail.com>
2023-03-10 12:34:25 +0200

    edit task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector("iframe");
+const player = new Player(iframe);
+
+const time = player.on('timeupdate', throttle(function (data) {
+    localStorage.setItem("videoplayer-current-time", data);
+}, 1000));
+
+
+currentTimeFunction();
+
+function currentTimeFunction() {
+    const timeValue = localStorage.getItem("videoplayer-current-time");
+    if (timeValue) {
+        const currentTime = timeValue['seconds'];
+        player.setCurrentTime(currentTime);
+    }
+};
+    
+
 
